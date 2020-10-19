cbcd2b3e7e103f6971b3f1df453db15c8986fc5f
eliasongun <eongun40@gmail.com>
2020-10-19 13:28:08 +0200

    readSudoku methode fertig

--- Sudoku_StudentStub/Sudoku_StudentStub/src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
+++ Sudoku_StudentStub/Sudoku_StudentStub/src/net/htlgrieskirchen/pos3/sudoku/SudokuSolver.java
@@ -1,6 +1,8 @@
 package net.htlgrieskirchen.pos3.sudoku;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /* Please enter here an answer to task four between the tags:
  * <answerTask4>
@@ -8,14 +10,31 @@ import java.io.File;
  * </answerTask4>
  */
 public class SudokuSolver implements ISodukoSolver {
+
     public SudokuSolver() {
         //initialize if necessary
     }
 
     @Override
     public final int[][] readSudoku(File file) {
-        // implement this method
-        return new int[0][0]; // delete this line!
+          int [] [] array = new int [9][9];
+        try {
+            array = Files.lines(file.toPath())
+                    .map(s -> s.split(";"))
+                    .map(a -> new int[]{Integer.parseInt(a[0]),
+                        Integer.parseInt(a[1]),
+                        Integer.parseInt(a[2]),
+                        Integer.parseInt(a[3]),
+                        Integer.parseInt(a[4]),
+                        Integer.parseInt(a[5]),
+                        Integer.parseInt(a[6]),
+                        Integer.parseInt(a[7]),
+                        Integer.parseInt(a[8])})
+                    .toArray(int[][]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return array;
     }
 
     @Override
@@ -29,7 +48,7 @@ public class SudokuSolver implements ISodukoSolver {
         // implement this method
         return new int[0][0]; // delete this line!
     }
-    
+
     @Override
     public int[][] solveSudokuParallel(int[][] rawSudoku) {
         // implement this method
